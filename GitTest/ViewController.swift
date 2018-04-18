69637dcd8800eb9ca41ee8ef4e2a98675a6e6533
Jorge Raul Solis Rangel <JorgeRaul@macbook-pro-de-jorge.local>
2018-04-17 21:53:55 -0500

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Jorge Raul Solis Rangel on 17/04/18.
+//  Copyright © 2018 Jorge Raul Solis Rangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
